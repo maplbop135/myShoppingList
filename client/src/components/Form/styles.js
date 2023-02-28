377ea1e2165ef7818a5df2db09d2bed864eceeda
maplbop135 <maplbop135@naver.com>
2023-02-28 13:55:34 +0900

    mobile-friendly UI

--- client/src/components/Form/styles.js
+++ client/src/components/Form/styles.js
@@ -8,6 +8,7 @@ export default makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
+    marginBottom: 20
   },
   form: {
     display: 'flex',
--- client/src/components/RecipesPage.js
+++ client/src/components/RecipesPage.js
@@ -1,14 +1,14 @@
 import { Container, Grow, Grid } from '@material-ui/core';
-import Recipes from './Recipes/Recipes'
-import Form from './Form/Form'
+import Recipes from './Recipes/Recipes';
+import Form from './Form/Form';
 
 export default function RecipesPage({ currentId, setCurrentId }){
     return (
-        <div className='container flex'>
+        <div className='recipe-container'>
             <Container maxWidth="lg">
                 <Grow in>
                     <Container>
-                        <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                        <Grid className="device" container justifyContent="space-between" alignItems="stretch" spacing={3}>
                             <Grid item xs={12} sm={7}>
                                 <Recipes setCurrentId={setCurrentId} />
                             </Grid>
